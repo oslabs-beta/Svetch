6721879aad4abf02ba9d5fadab8c4aa01c62fedd
Matthew Lapeer <matthew.lapeer@gmail.com>
2022-05-02 08:03:44 -0700

    updated utilities and restructured Directory

---
+++ src/utils/canvasUtility.js
@@ -0,0 +1,29 @@
+export default {
+  createChild: (name, type, parent) => {
+    console.log(name, type, parent)
+    canvas.update(c => {
+      c[parent].children.push(name);
+      c[name] = {children:[], 'scriptId': type}
+      return c;
+    });
+  },
+  removeChild: (name, parent) => {
+    canvas.update(c => {
+      const index = c[parent].children.indexOf(name);
+      c[parent].children.splice(index, 1);
+      return c;
+    });
+  },
+  addChild: (name, parent) => {
+    c[parent].children.push(name);
+    return c;
+  },
+  deleteChild: (name, parent) => {
+    canvas.update(c => {
+      const index = c[parent].children.indexOf(name);
+      c[parent].children.splice(index, 1);
+      delete c[name];
+      return c;
+    });
+  }
+}
