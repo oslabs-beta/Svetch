1631a914a1e26de0e7e02551732e9a6cf522e63d
Matthew Lapeer <matthew.lapeer@gmail.com>
2022-05-19 14:02:24 -0700

    download fix

--- src/routes/fileCreate.js
+++ src/routes/fileCreate.js
@@ -4,10 +4,10 @@ import fse from 'fs-extra'
 
 export async function post({ request }) {
     
-    // const cookies = cookie.parse(request.headers.get('cookie') || '');
-    // const requestData = await request.json();
-    // const {name, data, folder, sessionId} = requestData;
-    // const id = sessionId || cookies.session_id;
+    const cookies = cookie.parse(request.headers.get('cookie') || '');
+    const requestData = await request.json();
+    const {name, data, folder, sessionId} = requestData;
+    const id = sessionId || cookies.session_id;
    
     // if (!fs.existsSync(`${id}`)) {   
     //     fs.mkdirSync(`${id}`);
