1edd6476be84c022968945a7610df4b4c1d2241e
Matthew Lapeer <matthewlapeer@gmail.com>
2023-02-02 16:42:49 -0500

    update github export logic and add comments to exportProject.js

--- src/routes/exportProject.js
+++ src/routes/exportProject.js
@@ -1,132 +1,156 @@
 import { Octokit } from 'octokit';
 import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods';
-import fs from 'fs';
 import axios from 'axios';
 import fileUtility from '../utils/fileUtility';
-import { repoName } from '../store.js';
 
-export let myFakeUser = 'fake';
 export async function post({ request }) {
+  // Parse body of incoming request object
   const body = await request.json();
+
+  // Store the oauth token
   const token = body.token;
+
+  // Store the repo owner information
   const owner = body.user.login;
-  const sessionId = body.sessionId;
-  
-  let repo;
-  const unsubscribe = repoName.subscribe((val) => repo = val);
-	unsubscribe();
-  repo = repo.split(' ').join('-');
-  
-  const CustomOctokit = Octokit.plugin(restEndpointMethods);
-  const octokit = new CustomOctokit({ auth: token });
-  
-  // GitHub API helper functions
-  const createBlob = async (absolutePath, path) => {
-    const content = fs.readFileSync(absolutePath, { encoding: 'utf8' });
-    const response = await octokit.rest.git.createBlob({
-      owner,
-      repo,
-      content,
-    });
-    return {...response.data, path};
-  }
-  
-  const createCommit = async (commitSha, treeSha) => {
-    const response = await octokit.rest.git.createCommit({
-      owner,
-      repo,
-      message: 'Svetch Initial Commit',
-      parents: [],
-      tree: treeSha,
-    });
-    return response.data.sha;
-  }
-  
-  const createGitTree = async (tree, commitSha) => {
-    const response = await octokit.rest.git.createTree({
-      owner,
-      repo,
-      base_tree: commitSha,
-      tree
-    });
-    return response.data.sha;
-  }
 
-  const createTreeStructure = async (blobs) => {
-    const tree = [];
-    blobs.forEach(({ path, sha }) => tree.push({ 
-      path, 
-      mode: '100644',
-      type: 'blob',
-      sha }));
-    return tree;
-  }
+  // Store the repo name
+  const repo = body.repoName.replace(/\s+/g,'-');
+  
+    // Store a custom Octokit object with rest API plugin
+    const CustomOctokit = Octokit.plugin(restEndpointMethods);
 
-  const getBlobs = async (directory) => {
-    let blobs = [];
-    const dirEntries = fs.readdirSync(directory, {
-      withFileTypes: true,
-    });
+    // Store an initialized Octokit (pseudoclassical) object
+    const octokit = new CustomOctokit({ auth: token });
     
-    for (let dirEntry of dirEntries) {
-      const { name } = dirEntry;
-      const absolutePath = `${directory}/${name}`;
-      const relativePath = absolutePath.slice(sessionId.length + 1);
-
-      // If the dirEntry at the absolutePath contains file content, then add it to the repo at its relativePath
-      if (fs.statSync(absolutePath).isFile()) {
-        const newBlob = await createBlob(absolutePath, relativePath);
-        blobs.push(newBlob);
-      }
-
-      // Otherwise get the contents of the dirEntry at the absolutePath
-      else {
-        const newBlobs = await getBlobs(absolutePath);
-        blobs.push(...newBlobs)}
+    // GitHub API helper functions
+    const createBlob = async ({ relativePath, fileContent }, encoding) => {
+      // Store the API response containing git blob data
+      const response = await octokit.rest.git.createBlob({
+        owner,
+        repo,
+        content: fileContent,
+        encoding
+      });
+  
+      // Return a new object containing the git blob and relative file path
+      return { ...response.data, path: relativePath };
     }
+    
+    const createCommit = async (treeSha) => {
+      // Store the API response containing the new commit sha
+      const response = await octokit.rest.git.createCommit({
+        owner,
+        repo,
+        message: 'Svetch Initial Commit',
+        parents: [],
+        tree: treeSha,
+      });
+  
+      // Return the commit sha
+      return response.data.sha;
+    }
+    
+    const createGitTree = async (tree, commitSha) => {
+      // Store the API reponse containing the new git tree
+      const response = await octokit.rest.git.createTree({
+        owner,
+        repo,
+        base_tree: commitSha,
+        tree
+      });
+  
+      // Return the sha for the created git tree
+      return response.data.sha;
+    }
+  
+    const createTreeStructure = async (blobs) => {
+      // Declare an array to store the objects specifying hierarchy between files
+      const tree = [];
+  
+      // Iterate over the blobs and push a hierarchy object into the tree array for each
+      for (const { path, sha } of blobs) { 
+        tree.push({ 
+          path, 
+          mode: '100644',
+          type: 'blob',
+          sha });
+      }
+  
+      // Return the array defining the tree structure
+      return tree;
+    }
+
+  const getBlobs = async (files) => {
+    // Store the promises as they are created
+    const promises = [];
+
+    for (const file of files) {
+      // Store a new blob from the helper function
+      const newBlob = createBlob(file, (file.encoding || 'utf-8'));
+
+      // Add the new blob promise to the array
+      promises.push(newBlob);
+    }
+
+    // Await all blob promises to resolve to blob objects
+    const blobs = await Promise.all(promises);
+
+    // Return the git blob objects
     return blobs;
   };
 
   const getLastCommitSha = async () => {
+    // Get the repository 'main' branch metadata
     const branch = await octokit.rest.repos.getBranch({
       owner,
       repo,
       branch: 'main',
     });
+
+    // Return the sha of previous commit (initial commit from repository creation)
     return branch.data.commit.sha;
   }
   
   const updateReference = async (commitSha) => {
-    await octokit.rest.git.updateRef({
+    // return promise for API request to update git branch reference
+    return octokit.rest.git.updateRef({
       owner,
       repo,
       ref: 'heads/main',
       sha: commitSha,
       force: true
     });
-    return;
   }
 
   try {
     // Create new GitHub repo named the value of repo 
     await octokit.rest.repos.createForAuthenticatedUser({ name: repo, auto_init: true });
-    // Create project files in folder unique to user, copy files from template
-    await fileUtility.createFile(sessionId);
-    // Get git blobs for the commit
-    const blobs = await getBlobs(sessionId);
+   
+    // Create component files from user prototype
+    const componentFiles = fileUtility.createFile();
+
+    // Get the static project files
+    const projectFiles = await axios.get('https://app.svetch.io/api/projectSkeleton')
+      .then(({ data }) => JSON.parse(data))
+      .then(({ files }) => files);
+
+    // Get git blobs from the project and component files for the commit
+    const blobs = await getBlobs([ ...projectFiles, ... componentFiles ]);
+
     // Create the tree structure for the blobs
     const tree = await createTreeStructure(blobs);
+
     // Get the sha from the last commit (the inital commit when the repo was created)
     const commitSha = await getLastCommitSha();
+
     // Create a git tree and get the corresponding sha
     const treeSha = await createGitTree(tree, commitSha);
+
     // Create a new commit and get the corresponding sha
-    const newCommitSha = await createCommit(commitSha, treeSha);
+    const newCommitSha = await createCommit(treeSha);
+
     // Update the reference for the git branch
     await updateReference(newCommitSha);
-    // Delete the user folder
-    await axios.post('/fileDelete', {sessionId});
-
   } catch (err) {
     console.log('ERROR:', err)
   }
