1843c272362d00e18674338ab9d60afaca2a2c4c
Matthew Lapeer <matthewlapeer@gmail.com>
2023-02-17 09:20:07 -0500

    updates projectFiles API argmuments, 2 expected

--- api/projectFiles.js
+++ api/projectFiles.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
-export default async function get({ response }) {
+export default async function get(req, res) {
     //Find the absolute path of the json file
     const jsonPath = path.join(process.cwd(), 'json', 'data.json');
   
@@ -9,5 +9,5 @@ export default async function get({ response }) {
     const fileContent = await fs.readFile(jsonPath, { encoding: 'utf8' });
   
     //Return the content of the data in json format
-    return response.status(200).json(fileContent);
+    return res.status(200).json(fileContent);
   }
