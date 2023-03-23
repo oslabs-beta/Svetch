4ea674bf97350fccd9a1757820a63267e508a090
Matthew Lapeer <matthewlapeer@gmail.com>
2023-03-23 13:08:06 -0400

    renames Elements.svelte, updates import in index.svelte

--- src/lib/Guide.svelte
+++ src/lib/Guide.svelte
@@ -1,7 +1,8 @@
 <script>
   import { onMount } from 'svelte';
-  import { options } from '../store';
   import fileUtility from '../utils/fileUtility'
+  import libraryUtility from '../utils/libraryUtility';
+  import { library } from '../store';
 
   let name;
   let color;
@@ -9,15 +10,21 @@
   let errorMessage = '';
 
   $: {
-    if ($options.length) y = $options[$options.length - 1].y + 60;
+    const libraryItems = Object.keys($library);
+    if (libraryItems.length) {
+      const lastKey = libraryItems[libraryItems.length - 1];
+      y = $library[lastKey].rect.y + 60;
+    }
     else y = 20;
   }
 
   const handleSubmission = () => {
     errorMessage = '';
     const safeName = fileUtility.formatName(name.value);
-    for (let i = 0; i < $options.length; i++) {
-      if ($options[i].type == safeName) {
+    const safeColor = color.value || 'white';
+    const libraryKeys = Object.keys($library);
+    for (let i = 0; i < libraryKeys.length; i++) {
+      if (libraryKeys[i] == safeName) {
         errorMessage = 'Please provide a unique name';
         return;
       }
@@ -31,16 +38,7 @@
       return;
     }
   
-    const newOption = {
-      x: 20,
-      y: y,
-      width: 150,
-      height: 50,
-      type: safeName,
-      color: color.value || 'white',
-      deletable: true,
-    };
-    options.update((n) => [...n, newOption]);
+    libraryUtility.create(safeName, safeColor, y);
     name.value = '';
     color.value = '';
     return;
@@ -54,54 +52,54 @@
       let timeoutId;
       return () => {
         clearTimeout(timeoutId);
-        quickstart.classList.remove("hideScroll");
+        quickstart.classList.remove('hideScroll');
         timeoutId = setTimeout(() => {
-          quickstart.classList.add("hideScroll");
+          quickstart.classList.add('hideScroll');
         }, 300);
       }
     };
 
     const cb = debouce();
 
-    quickstart.addEventListener("scroll", cb)
+    quickstart.addEventListener('scroll', cb)
   });
 </script>
 
 <h1>Create New Component</h1>
-<label for="componentName">Component Name:</label>
+<label for='componentName'>Component Name:</label>
 <input
   bind:this={name}
-  type="text"
-  id="componentName"
-  name="componentName"
-  value=""
+  type='text'
+  id='componentName'
+  name='componentName'
+  value=''
 />
 
 {#if errorMessage !== ''}
-  <h5 id="errorMessage">{errorMessage}</h5>
+  <h5 id='errorMessage'>{errorMessage}</h5>
 {/if}
-<label for="color-select">Color:</label>
-<select bind:this={color} name="color" id="color-select">
-  <option value="">-- Please choose a color --</option>
-  <option value="#81a1c1">Blue</option>
-  <option value="#a3be8c">Green</option>
-  <option value="#d7dce0">Grey</option>
-  <option value="#e9897c">Orange</option>
-  <option value="#7879FF">Purple</option>
-  <option value="#EB4748">Red</option>
-  <option value="white">White</option>
-  <option value="#F2F270">Yellow</option>
+<label for='color-select'>Color:</label>
+<select bind:this={color} name='color' id='color-select'>
+  <option value=''>-- Please choose a color --</option>
+  <option value='#81a1c1'>Blue</option>
+  <option value='#a3be8c'>Green</option>
+  <option value='#d7dce0'>Grey</option>
+  <option value='#e9897c'>Orange</option>
+  <option value='#7879FF'>Purple</option>
+  <option value='#EB4748'>Red</option>
+  <option value='white'>White</option>
+  <option value='#F2F270'>Yellow</option>
 </select>
 
 <button on:click={() => handleSubmission()}>Submit</button>
-<h1 style="text-align: center;">Quickstart Guide</h1>
+<h1 style='text-align: center;'>Quickstart Guide</h1>
 <hr />
-<div class="quickstart hideScroll">
+<div class='quickstart hideScroll'>
   <h3>Getting Started</h3>
   <ul>
     <li>
       The dotted grid to the right is the Canvas, and represents your
-      <span class="codeSnippet">`index.svelte`</span> file.
+      <span class='codeSnippet'>`index.svelte`</span> file.
     </li>
     <li>
       To add components to the Canvas, enter the name of your component and
@@ -109,7 +107,7 @@
     </li>
     <li>
       When you click the Submit button, the new component will appear in the
-      components menu. Click the component and it will appear in the Canvas, as
+      components library (left of Canvas). Click the component and it will appear in the Canvas, as
       well as the Code Preview and Directory panes.
     </li>
   </ul>
@@ -117,7 +115,7 @@
   <ul>
     <li>
       You can move a component by clicking down inside of the box and dragging
-      it around the Canvas, then release your mouse to finalize it's position.
+      it around the Canvas, then release your mouse to finalize its position.
     </li>
     <li>
       You can also resize your components by clicking down and dragging the
@@ -133,7 +131,7 @@
     </li>
     <li>
       You can view a tree diagram of your component hierarchy by clicking the
-      toggle button in the upper right corner of the Canvas. Once there, you can
+      toggle in the upper right corner of the Canvas. When viewing the tree diagram, you can
       click on the name of your components to expand the tree and see the
       components nested structure.
     </li>
@@ -143,7 +141,7 @@
     <li>
       To preview a component's code, simply click on its box inside of the
       Canvas or its name in the Directory pane, and the Code Preview pane will update (Note that the entire Canvas
-      defaults to <span class="codeSnippet">`index.svelte`</span>).
+      defaults to <span class='codeSnippet'>`index.svelte`</span>).
     </li>
   </ul>
 </div>
