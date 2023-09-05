fce8f32aabb0d8fc319e6fdad499b6c67053638e
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-09-05 13:08:07 -0400

    update web server command for gh action workflow

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -73,7 +73,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run preview',
-    url: 'http://localhost:3000',
+    port: 3000,
     reuseExistingServer: !process.env.CI,
   },
 });
