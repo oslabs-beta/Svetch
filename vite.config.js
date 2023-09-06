f200c3c1c55428d63c1a8be030d642d23f1f2e94
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-09-06 15:33:37 -0400

    update dependencies from svelte-kit v0 (next) to v1, update routes directory and app.html for v1 changes

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    include: ['highlight.js', 'highlight.js/lib/core']
+  },
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..']
+    }
+  }
+});
