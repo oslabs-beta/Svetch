5a1637072be5af941e36afa32cc2b918db994a6b
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-09-09 14:11:07 -0400

    update directory of api, update projectFiles logic for v1

--- src/routes/api/projectFiles/+server.js
+++ src/routes/api/projectFiles/+server.js
@@ -1,13 +1,13 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
-export default async function get(req, res) {
+// eslint-disable-next-line import/prefer-default-export
+export async function GET() {
   // Find the absolute path of the json file
   const jsonPath = path.join(process.cwd(), 'json', 'data.json');
 
   // Read content of the data.json file
   const fileContent = await fs.readFile(jsonPath, { encoding: 'utf8' });
-
   // Return the content of the data in json format
-  return res.status(200).json(fileContent);
+  return new Response(fileContent)
 }
