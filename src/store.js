7133b1a88ec3b3c72373e0dca48daf0c233d3f2e
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-09-11 12:44:27 -0400

    update comments to silence linter for vite config and store.js

--- src/store.js
+++ src/store.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import { writable } from 'svelte/store';
 
 export const canvas = writable({
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
