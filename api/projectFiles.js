a25047dce2ff9124428dbe80e23a02b9f4a2dd7b
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-09-11 12:49:00 -0400

    move api directory, update logic to Vercel Build Output API v3

--- api/projectFiles.js
+++ api/projectFiles.js
@@ -1,13 +1,13 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
-// eslint-disable-next-line import/prefer-default-export
-export async function GET() {
+export default async function handler(req, res) {
   // Find the absolute path of the json file
   const jsonPath = path.join(process.cwd(), 'json', 'data.json');
 
   // Read content of the data.json file
   const fileContent = await fs.readFile(jsonPath, { encoding: 'utf8' });
-  // Return the content of the data in json format
-  return new Response(fileContent)
+
+  // Return the content of the data
+  return res.status(200).send(fileContent);
 }
